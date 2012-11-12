fec8e415e49b644f36ab77f1f866207be89f243c
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2012-11-12 17:39:33 +0100

    Re-add HWLOC as optional dependency since new default target of Buildyard does not rerun configure for it

--- Lunchbox.cmake
+++ Lunchbox.cmake
@@ -2,5 +2,5 @@
 set(LUNCHBOX_PACKAGE_VERSION 1.5.0)
 set(LUNCHBOX_REPO_URL https://github.com/Eyescale/Lunchbox.git)
 set(LUNCHBOX_REPO_TAG master)
-set(LUNCHBOX_DEPENDS eyescale eyescalePorts DNSSD REQUIRED Boost)
+set(LUNCHBOX_DEPENDS eyescale eyescalePorts HWLOC DNSSD REQUIRED Boost)
 set(LUNCHBOX_BOOST_COMPONENTS serialization)
