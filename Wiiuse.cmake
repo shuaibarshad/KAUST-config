69db842769092b57d9ea2807eb3aaed900f69239
Jafet Villafranca <jafet.villafrancadiaz@epfl.ch>
2012-11-20 15:36:00 +0100

    Fixed dependencies and used proper naming

--- Wiiuse.cmake
+++ Wiiuse.cmake
@@ -1,6 +1,6 @@
 
 set(WIIUSE_PACKAGE_VERSION 0.14)
 set(WIIUSE_REPO_URL https://github.com/jafyvilla/wiiuse.git)
-set(WIIUSE_DEPENDS REQUIRED Bluez)
+set(WIIUSE_DEPENDS REQUIRED VRPN)
 set(WIIUSE_FORCE_BUILD ON) # until wiiuse finder is version-aware?
 set(WIIUSE_OPTIONAL ON)
