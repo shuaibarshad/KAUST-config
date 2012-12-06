02ab5777600571ad12313efbb59335acd3619a32
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2012-12-06 12:41:14 +0100

    Merge cleanup

--- hwsd.cmake
+++ hwsd.cmake
@@ -2,6 +2,5 @@
 set(HWSD_PACKAGE_VERSION 1.0.0)
 set(HWSD_REPO_URL https://github.com/Eyescale/hwsd.git)
 set(HWSD_REPO_TAG master)
-set(HWSD_ROOT_VAR HWSD_ROOT)
 set(HWSD_DEPENDS Boost HWLOC OpenGL X11 REQUIRED Lunchbox)
 set(HWSD_BOOST_COMPONENTS program_options)
