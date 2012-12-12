85b6ebac59c6f0958f69d143fc9897452d5dc4cd
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2012-12-12 17:04:04 +0100

    Use Qt4 (network interface subnet filtering for autoconfig)

--- Equalizer.cmake
+++ Equalizer.cmake
@@ -1,8 +1,9 @@
 
 set(EQUALIZER_PACKAGE_VERSION 1.5.0)
 set(EQUALIZER_DEPENDS REQUIRED VMMLIB Lunchbox Collage OpenGL Boost
-                      OPTIONAL X11 hwsd GLStats HWLOC)
-set(EQUALIZER_BOOST_COMPONENTS "system regex date_time serialization")
+                      OPTIONAL X11 hwsd GLStats HWLOC Qt4)
+set(EQUALIZER_BOOST_COMPONENTS "program_options")
+set(EQUALIZER_QT4_COMPONENTS "QtNetwork")
 set(EQUALIZER_ROOT_VAR EQ_ROOT)
 set(EQUALIZER_REPO_URL https://github.com/Eyescale/Equalizer.git)
 set(EQUALIZER_REPO_TAG master)
