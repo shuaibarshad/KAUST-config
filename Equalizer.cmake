067f77bb2851432bcbba07d7b6240a368733cc44
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-01-18 10:15:35 +0100

    Update Equalizer dependencies

--- Equalizer.cmake
+++ Equalizer.cmake
@@ -1,7 +1,7 @@
 
 set(EQUALIZER_PACKAGE_VERSION 1.5.0)
-set(EQUALIZER_DEPENDS REQUIRED VMMLIB Lunchbox Collage OpenGL Boost
-                      OPTIONAL X11 hwsd GLStats HWLOC Qt4)
+set(EQUALIZER_DEPENDS REQUIRED vmmlib Lunchbox Collage OpenGL Boost
+                      OPTIONAL X11 hwsd GLStats HWLOC Qt4 OpenSceneGraph)
 set(EQUALIZER_BOOST_COMPONENTS "program_options")
 set(EQUALIZER_QT4_COMPONENTS "QtNetwork")
 set(EQUALIZER_ROOT_VAR EQ_ROOT)
