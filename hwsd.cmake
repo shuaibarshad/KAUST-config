e5b29d8aad043541d6afc4c6855aac1bbcb82454
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-02-15 18:16:26 +0100

    Update dependencies

--- hwsd.cmake
+++ hwsd.cmake
@@ -2,6 +2,6 @@
 set(HWSD_PACKAGE_VERSION 1.0.0)
 set(HWSD_REPO_URL https://github.com/Eyescale/hwsd.git)
 set(HWSD_REPO_TAG master)
-set(HWSD_DEPENDS Boost OpenGL Qt4 X11 REQUIRED Lunchbox)
+set(HWSD_DEPENDS OpenGL Qt4 X11 REQUIRED Lunchbox Boost)
 set(HWSD_BOOST_COMPONENTS "program_options regex")
 set(HWSD_QT4_COMPONENTS "QtNetwork")
