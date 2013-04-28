d163446d6f04489bfa257528c2b84c82371b8961
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-04-28 16:41:02 +0200

    Split Eq/Co DEP_DEPENDENCIES

--- Collage.cmake
+++ Collage.cmake
@@ -1,6 +1,7 @@
 
 set(COLLAGE_PACKAGE_VERSION 0.7.0)
 set(COLLAGE_DEPENDS OFED UDT REQUIRED Boost Lunchbox)
+set(COLLAGE_DEB_DEPENDS librdmacm-dev libibverbs-dev librdmacm-dev libudt-dev)
 set(COLLAGE_BOOST_COMPONENTS "system regex date_time serialization")
 set(COLLAGE_REPO_URL https://github.com/Eyescale/Collage.git)
 set(COLLAGE_REPO_TAG master)
