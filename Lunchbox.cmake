9c15fc89aee78f59041bfb31d3d650570eae109a
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2013-04-29 17:54:30 +0200

    Fix deb dependencies

--- Lunchbox.cmake
+++ Lunchbox.cmake
@@ -3,6 +3,6 @@ set(LUNCHBOX_PACKAGE_VERSION 1.7.5)
 set(LUNCHBOX_REPO_URL https://github.com/Eyescale/Lunchbox.git)
 set(LUNCHBOX_REPO_TAG master)
 set(LUNCHBOX_DEPENDS eyescale eyescalePorts hwloc DNSSD REQUIRED Boost)
-set(LUNCHBOX_DEB_DEPENDS libboost-all-dev libhwloc-dev
-  libavahi-compat-libdnssd-dev)
+set(LUNCHBOX_DEB_DEPENDS libboost-regex-dev libboost-serialization-dev
+  libhwloc-dev libavahi-compat-libdnssd-dev)
 set(LUNCHBOX_BOOST_COMPONENTS "regex serialization")
