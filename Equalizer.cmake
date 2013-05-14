50c99cd75a220028d1f08c2e6bb2989626358aee
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-05-14 11:44:44 +0200

    Tweak Equalizer config

--- Equalizer.cmake
+++ Equalizer.cmake
@@ -2,12 +2,14 @@
 set(EQUALIZER_PACKAGE_VERSION 1.5.0)
 set(EQUALIZER_DEPENDS
   REQUIRED vmmlib Lunchbox Collage OpenGL Boost BISON FLEX
-  OPTIONAL X11 hwsd GLStats hwloc OpenSceneGraph SAGE OpenCV)
+  OPTIONAL X11 hwsd GLStats hwloc OpenSceneGraph SAGE OpenCV PythonInterp)
 set(EQUALIZER_DEB_DEPENDS bison flex libboost-program-options-dev
   libx11-dev libgl1-mesa-dev libglewmx1.6-dev libspnav0
   libopenscenegraph-dev libopencv-dev)
 
 set(EQUALIZER_BOOST_COMPONENTS "program_options")
+set(EQUALIZER_OPENSCENEGRAPH_COMPONENTS "osgDB osgUtil")
+
 set(EQUALIZER_ROOT_VAR EQ_ROOT)
 set(EQUALIZER_REPO_URL https://github.com/Eyescale/Equalizer.git)
 set(EQUALIZER_REPO_TAG master)
