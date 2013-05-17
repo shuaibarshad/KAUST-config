336dcfc6eea20223fbfa0296e4f726265aaae284
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-05-17 13:58:50 +0200

    Revert broken bison/flex detection from Buildyard

--- Equalizer.cmake
+++ Equalizer.cmake
@@ -1,7 +1,7 @@
 
 set(EQUALIZER_PACKAGE_VERSION 1.5.0)
 set(EQUALIZER_DEPENDS
-  REQUIRED vmmlib Lunchbox Collage OpenGL Boost BISON FLEX
+  REQUIRED vmmlib Lunchbox Collage OpenGL Boost
   OPTIONAL X11 hwsd GLStats hwloc OpenSceneGraph SAGE OpenCV PythonInterp
   DisplayCluster MAGELLAN GLEW_MX)
 set(EQUALIZER_DEB_DEPENDS bison flex libboost-program-options-dev
