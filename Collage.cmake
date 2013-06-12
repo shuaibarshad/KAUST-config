2a57b413aca4964dccc9cb7f1b54615be66c0962
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2013-06-12 11:40:34 +0200

    Generic CI force build

--- Collage.cmake
+++ Collage.cmake
@@ -7,3 +7,4 @@ set(COLLAGE_DEB_DEPENDS librdmacm-dev libibverbs-dev librdmacm-dev libudt-dev
 set(COLLAGE_BOOST_COMPONENTS "system regex date_time serialization")
 set(COLLAGE_REPO_URL https://github.com/Eyescale/Collage.git)
 set(COLLAGE_REPO_TAG master)
+set(COLLAGE_FORCE_BUILD ${CI_BUILD})
--- OpenCV.cmake
+++ OpenCV.cmake
@@ -1,5 +1,8 @@
 
 set(OPENCV_PACKAGE_VERSION 2.0)
+if(${CI_BUILD})
+  return()
+endif()
 set(OPENCV_REPO_URL https://github.com/Itseez/opencv.git)
 set(OPENCV_REPO_TAG 2.4.5)
 set(OPENCV_CMAKE_INCLUDE "SYSTEM")
