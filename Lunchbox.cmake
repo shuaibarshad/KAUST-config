85c06dba973831ca67a4972a4844a2ff90d67dec
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-07-12 12:02:32 +0200

    Activate JPEG compressor

--- Lunchbox.cmake
+++ Lunchbox.cmake
@@ -2,7 +2,8 @@
 set(LUNCHBOX_PACKAGE_VERSION 1.8.0)
 set(LUNCHBOX_REPO_URL https://github.com/Eyescale/Lunchbox.git)
 set(LUNCHBOX_REPO_TAG master)
-set(LUNCHBOX_DEPENDS eyescale eyescalePorts hwloc DNSSD REQUIRED Boost)
+set(LUNCHBOX_DEPENDS eyescale eyescalePorts hwloc DNSSD LibJpegTurbo
+                     REQUIRED Boost)
 set(LUNCHBOX_DEB_DEPENDS libboost-regex-dev libboost-serialization-dev
   libhwloc-dev libavahi-compat-libdnssd-dev)
 set(LUNCHBOX_BOOST_COMPONENTS "regex serialization")
