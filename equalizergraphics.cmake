0ac064e2b85d2a829ecf09c946403b878b34f27b
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-08-06 15:45:22 +0200

    Config tweak

--- equalizergraphics.cmake
+++ equalizergraphics.cmake
@@ -2,7 +2,6 @@ set(EQUALIZERGRAPHICS_OPTIONAL true)
 set(EQUALIZERGRAPHICS_DEPENDS REQUIRED Equalizer EqDocs)
 set(EQUALIZERGRAPHICS_REPO_URL https://github.com/Eyescale/equalizergraphics.com.git)
 set(EQUALIZERGRAPHICS_REPO_TAG master)
-set(EQUALIZERGRAPHICS_SOURCE "${CMAKE_SOURCE_DIR}/src/equalizergraphics")
 
 set(EQUALIZERGRAPHICS_EXTRA
   CONFIGURE_COMMAND ${CMAKE_COMMAND} -E touch ${CMAKE_BINARY_DIR}/equalizergraphics/config.status
