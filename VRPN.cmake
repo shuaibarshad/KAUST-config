91bd3fd06ea672b51ce8ba18c1dd3f66d42b2ca4
Raphael Dumusc <raphael.dumusc@epfl.ch>
2013-08-20 15:28:13 +0200

    Removed incorrect dependency of Wiiuse on VRPN, and building VRPN with Wiiuse support by default

--- VRPN.cmake
+++ VRPN.cmake
@@ -3,3 +3,6 @@ set(VRPN_PACKAGE_VERSION 07.30)
 set(VRPN_REPO_URL https://github.com/BlueBrain/vrpn.git)
 # set(VRPN_FORCE_BUILD ON) # Until finder is version-aware
 set(VRPN_OPTIONAL ON)
+# Build with Wiiuse support by default
+set(VRPN_DEPENDS REQUIRED Wiiuse)
+set(VRPN_CMAKE_ARGS -DVRPN_USE_WIIUSE=ON -DVRPN_GPL_SERVER=ON)
--- Wiiuse.cmake
+++ Wiiuse.cmake
@@ -1,6 +1,6 @@
 
 set(WIIUSE_PACKAGE_VERSION 0.14)
 set(WIIUSE_REPO_URL https://github.com/jafyvilla/wiiuse.git)
-set(WIIUSE_DEPENDS REQUIRED VRPN BlueZ)
+set(WIIUSE_DEPENDS REQUIRED BlueZ)
 set(WIIUSE_FORCE_BUILD ON) # until wiiuse finder is version-aware?
 set(WIIUSE_OPTIONAL ON)
