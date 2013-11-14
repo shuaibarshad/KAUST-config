c925ec5bfad2d9c5e45b295bf67942d718531ca0
Madhu Srinivasan <madhu.srinivasan@kaust.edu.sa>
2013-11-14 17:04:47 +0300

    added piston config

---
+++ Piston.cmake
@@ -0,0 +1,7 @@
+set(PISTON_PACKAGE_VERSION 0.1)
+set(PISTON_DEPENDS OpenGL MPI VTK Qt4)
+set(PISTON_BUILD_DEMO ON)
+set(PISTON_USE_CUDA ON)
+set(PISTON_REPO_URL https://github.com/losalamos/PISTON.git)
+set(PISTON_REPO_TAG master)
+set(PISTON_SOURCE "${CMAKE_SOURCE_DIR}/src/Piston")
