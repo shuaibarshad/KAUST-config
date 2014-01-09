18df6026a8052e0a2a815f276a21d5441825261c
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-01-09 16:55:40 +0100

    Switch to OS X-compilable Poppler version

--- Poppler.cmake
+++ Poppler.cmake
@@ -1,7 +1,7 @@
 
 set(POPPLER_PACKAGE_VERSION 0.24)
-set(POPPLER_REPO_URL git://git.freedesktop.org/git/poppler/poppler)
-set(POPPLER_REPO_TAG 0af3e00)
+set(POPPLER_REPO_URL https://github.com/BlueBrain/Poppler.git)
+set(POPPLER_REPO_TAG bbp)
 set(POPPLER_OPTIONAL ON)
 set(POPPLER_CMAKE_ARGS -DENABLE_UTILS=OFF -DENABLE_GLIB=OFF)
 set(POPPLER_DEPENDS REQUIRED Qt4)
